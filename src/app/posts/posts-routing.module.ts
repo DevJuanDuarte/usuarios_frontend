c89462762fe386842a2169d50119cb0979777a84
Juan Duarte <juan.devcontact@gmail.com>
2024-11-06 02:35:16 -0500

    Definiendo rutas

--- src/app/posts/posts-routing.module.ts
+++ src/app/posts/posts-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { NewComponent } from './pages/new/new.component';
+import { ListComponent } from './pages/list/list.component';
+import { PostComponent } from './pages/post/post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'new', component: NewComponent },
+      { path: 'list', component: ListComponent },
+      { path: ':id', component: PostComponent },
+      { path: '**', redirectTo: 'list' }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
